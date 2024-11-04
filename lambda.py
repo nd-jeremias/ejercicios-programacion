503339a6b6db381bdcedce6546f5c0f362ebe306
nd-jeremias <nd.jeremias@live.com>
2024-11-04 11:49:57 -0300

    Ejercicios Lambda

---
+++ lambda.py
@@ -0,0 +1,22 @@
+#1
+""" funcion = lambda aumento: aumento*1.10
+print((lambda aumento: aumento*1.10)(10))
+print(funcion(10)) """
+
+#2
+""" print((lambda edad: True if edad > 17 else False)(20)) """
+
+#3
+""" print((lambda par: True if par %2 == 0 else False)(5)) """
+
+#4
+""" print((lambda positivo: True if positivo > 0 else False)(-1)) """
+
+#5
+""" print((lambda importe: importe * 0.9)(100)) """
+
+#6
+""" print((lambda doble: doble*2)(147)) """
+
+#7
+""" print((lambda genero: "Femenino" if genero == "f" else "Masculino")("F".lower())) """
